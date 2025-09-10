83ee833984e61ccffb9562520511fd2042d10861
FarzadVav <farzad.vav.work@gmail.com>
2025-09-10 14:19:30 +0330

    add layout.css to playground

--- apps/playground/src/main.ts
+++ apps/playground/src/main.ts
@@ -1,25 +1,16 @@
-import "./style.css";
-import viteLogo from "/vite.svg";
-import { setupCounter } from "./counter.ts";
-import typescriptLogo from "./typescript.svg";
+window.addEventListener("load", () => {
+  try {
+    const themeHandler = (ev: MediaQueryListEvent | MediaQueryList) => {
+      const theme = ev.matches ? "dark" : "light";
+      document.querySelector("html")!.setAttribute("data-theme", theme);
+    };
 
-const app = document.querySelector<HTMLDivElement>("#app")!;
+    const darkModeQuery = window.matchMedia("(prefers-color-scheme: dark)");
 
-app.innerHTML = `
-  <div class="h-screen w-screen f-center flex-col">
-    <div class="f-align gap-12">
-      <a href="https://vite.dev" target="_blank">
-        <img src="${viteLogo}" class="size-32" alt="Vite logo" />
-      </a>
-      <a href="https://www.typescriptlang.org/" target="_blank">
-        <img src="${typescriptLogo}" class="size-32" alt="TypeScript logo" />
-      </a>
-    </div>
+    darkModeQuery.addEventListener("change", themeHandler);
 
-    <h1 class="text-5xl font-black mt-12">Vite + TypeScript</h1>
-    
-    <button class="btn btn-fill mt-6" id="counter"></button>
-  </div>
-`;
-
-setupCounter(app.querySelector<HTMLButtonElement>("#counter")!);
+    themeHandler(darkModeQuery);
+  } catch (err) {
+    console.log(err);
+  }
+});
