23f6fa5c0d7c03da5b03925514713b1d8c72aa97
FarzadVav <farzad.vav.work@gmail.com>
2025-09-11 13:47:25 +0330

    add other to playground

--- apps/playground/src/main.ts
+++ apps/playground/src/main.ts
@@ -2,12 +2,29 @@ import { createIcons, icons } from "lucide";
 createIcons({ icons });
 
 const kadouiElements = document.querySelectorAll("[data-kadoui=element]");
-
 Array.from(kadouiElements).forEach((elem) => {
   elem.addEventListener("click", () => {
     alert(`<${elem.localName} class="${elem.className}" />`);
   });
 });
+
+const toggleTextBtn = document.getElementById("toggle-text-btn");
+const toggleTextP = document.getElementById("toggle-text-p");
+function handleToggleText() {
+  if (!toggleTextBtn) return;
+
+  if (Array.from(toggleTextP?.classList || []).includes("ignore")) {
+    toggleTextBtn.innerHTML = "Toggle text to ignore";
+    toggleTextP?.classList.remove("ignore");
+    toggleTextP?.classList.add("not-ignore");
+  } else {
+    toggleTextBtn.innerHTML = "Toggle text to not-ignore";
+    toggleTextP?.classList.remove("not-ignore");
+    toggleTextP?.classList.add("ignore");
+  }
+}
+handleToggleText();
+toggleTextBtn?.addEventListener("click", handleToggleText);
 
 window.addEventListener("load", () => {
   try {
